d845d7a0bd428b7200e8031aa0fce5d660b935b1
oktoala <okta.yoga.syah@gmail.com>
2023-10-14 00:05:33 +0800

    Init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Tauri + React + Typescript
+
+This template should help get you started developing with Tauri, React and Typescript in Vite.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+### Install
+
+Recommended using [pnpm](https://pnpm.io/)
+
+- Clone This Repo then install dependencies
+
+```bash
+pnpm i
+```
+
+- Run it
+
+```bash
+pnpm tauri dev
+```
+
+### Install Npm Package
+
+```bash
+pnpm i [package-name]
+```
+
+### Install Cargo Crate
+
+```bash
+cd src-tauri
+cargo add [crate-name]
+```
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+// Prevents additional console window on Windows in release
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use local_ip_address::local_ip;
+// use core::net::IpAddr;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet() -> String {
+      match local_ip() {
+        Ok(ip) => format!("{}", ip),
+        Err(err) => format!("{}", err),
+    }
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_app::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
